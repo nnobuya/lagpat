8d5b46ee38713a467fd60a5a10bc3584e02a1a6e
Nobuya Nishimura <nishimura.nobuya@gmail.com>
2015-10-28 07:23:44 +0000

    update

--- movie/mk_anim.sh
+++ movie/mk_anim.sh
@@ -1,7 +1,44 @@
 #! /bin/sh
 
 
-n=10
+nskip=10
+nini=11
+#nfin=100
+
+n=$nini
+m=0
+nout=11
+
+while read time
+do
+    if [ $n -eq $nout ]; then
+	m=$(( $m + 1 ))
+	no1=`printf "%.4d" $m`
+	no2=`printf "%.4d" $n`
+
+	gnuplot<<EOF
+se te post eps color font 'Times, 18'
+#se te pdf font 'Times, 18'
+se ou "./eps/pt.${no1}.eps"
+se si sq
+se xr [0:2000]
+se yr [0:2000]
+se xtics 0, 500
+se ytics 0, 500
+se mxtics 5
+se mytics 5
+se cbr [0.15:0.55]
+#se palette define (0 'red', 0.5 'blue')
+se tit '$time ms'
+p '../res/anim/anim_${no2}.dat' u (\$1*1.e-5):(\$2*1.e-5):6 lc palette ps 0.2 pt 7 noti
+EOF
+	nout=$(( $nout + $nskip ))
+    fi
+
+    n=$(( $n + 1 ))
+done < ../res/anim_set.dat
+
+exit
 
 until [ $n -gt 380 ]
 #until [ $n -gt 10 ]
