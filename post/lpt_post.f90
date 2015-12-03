459b3b670799235260013679c0b53de288896490
Nobuya NISHIMURA <nobuya@Nobuyas-MBP.home>
2015-12-03 10:30:16 +0000

    update

--- post/lpt_post.f90
+++ post/lpt_post.f90
@@ -5,16 +5,22 @@ program lpt_post
   integer, parameter:: ndim = 3
   integer, parameter:: ndt_ex = 100
   real(8), parameter:: ti_ini = 1.d-2, ti_fin = 1.d4
+  real(8), parameter:: te_nse = 9.d9
 
+  !..main
   integer:: npt = 10000
   integer:: istg, ipt, ndt, idt
   real(8):: dt, dt_ex
-  integer, allocatable:: ist_pt(:,:)
+  integer, allocatable:: ist_pt(:,:), ndt_pt(:)
   real(8), allocatable:: ti(:), &
        & de_pt(:,:), te_pt(:,:), en_pt(:,:), &
        & ye_pt(:,:), x_pt(:,:,:), v_pt(:,:,:)
   real(8):: ti_ex(1:ndt_ex), de_ex, te_ex, en_ex, ye_ex
 
+  integer:: n_nse
+  real(8):: ti_nse, de_nse, en_nse, ye_nse
+
+  real(8):: h
   real(4):: dt_in, ti_in
   real(4), allocatable:: de_in(:), te_in(:), en_in(:), ye_in(:), &
        & x_in(:,:), v_in(:,:)
@@ -63,7 +69,7 @@ program lpt_post
   allocate(de_in(1:npt), te_in(1:npt), en_in(1:npt), ye_in(1:npt), &
        & x_in(1:ndim,1:npt), v_in(1:ndim,1:npt))
 
-  allocate(ti(1:ndt), ist_pt(1:npt,1:ndt), &
+  allocate(ti(1:ndt), ist_pt(1:npt,1:ndt), ndt_pt(1:npt), &
        & de_pt(1:npt,1:ndt), te_pt(1:npt,1:ndt), &
        & en_pt(1:npt,1:ndt), ye_pt(1:npt,1:ndt), &
        & x_pt(1:ndim,1:npt,1:ndt), v_pt(1:ndim,1:npt,1:ndt))
@@ -98,6 +104,18 @@ program lpt_post
 
   deallocate(de_in, te_in, en_in, ye_in)
 
+
+  ndt_pt(1:npt) = ndt
+  do ipt = 1, npt
+     do idt = 1, ndt
+        if (ist_pt(ipt,idt) /= 0) then
+           ndt_pt(ipt) = idt
+           cycle
+        end if
+     end do
+  end do
+
+
   do ipt = 1, npt
      write(61,'(1p, *(e14.5))') &
           & de_pt(ipt,1)  , te_pt(ipt,1)  , ye_pt(ipt,1), &
@@ -113,17 +131,39 @@ program lpt_post
 
      if (mod(ipt,1000) == 0) write(*,'(2i10)') ipt, npt
 
-     write(ofile,'("./tracer/hydro_", i5.5, ".dat")') ipt
+     !..original hydro data
+     write(ofile,'("./tracer/hydro_", i5.5, ".org")') ipt
      open(60, file = ofile, action = 'write')
 
-     do idt = 1, ndt
-        if (ist_pt(ipt,idt) == 0) then
-           write(60,'(1p, *(e15.7))') ti(idt), &
-                & de_pt(ipt,idt), te_pt(ipt,idt), ye_pt(ipt,idt)
-        end if
+     do idt = 1, ndt_pt(ipt)
+        write(60,'(1p, *(e15.7))') ti(idt), &
+        & de_pt(ipt,idt), te_pt(ipt,idt), ye_pt(ipt,idt)
      end do
-
      close(60)
+
+
+     if (maxval(te_pt(ipt,1:ndt)) < te_nse) then
+        print *, 'check later'
+        cycle
+     else
+
+        do idt = ndt_pt(ipt), 1, -1
+           if (te_pt(ipt,idt) >= te_nse) then
+              n_nse = idt
+           end if
+        end do
+
+        h = (te_nse - te_pt(ipt,idt)) &
+        & /(te_pt(ipt,n_nse + 1) - te_pt(ipt,n_nse))
+
+        ti_nse = ti(n_nse)        + h *(ti(n_nse + 1)      - ti(n_nse))
+        de_nse = de_pt(ipt,n_nse) &
+             & + h *(de_pt(ipt,n_nse + 1) - de_pt(ipt,n_nse))
+        ye_nse = ye_pt(ipt,n_nse) &
+             & + h *(ye_pt(ipt,n_nse + 1) - ye_pt(ipt,n_nse))
+
+     end if
+
   end do
 
 
