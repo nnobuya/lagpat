feb613c12ad31db3ba4c5b69d6bfb8e1be8f7065
Nobuya Nishimura <nishimura.nobuya@gmail.com>
2015-12-05 18:03:28 +0000

    update

---
+++ post/mk_hydro_in.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+import subprocess
+
+dir_path = './hydro.in'
+
+subprocess.call(['rm', '-rf', dir_path])
+subprocess.call(['mkdir', dir_path])
+
+
+no2 = 0
+
+for line in open('./in/pt_list.dat'):
+    no2 += 1
+
+    no1 = int(line)
+
+    if no1 <= 10000:
+        in_path = './tracer.1'
+    else:
+        in_path = './tracer.2'
+        no1 = no1 - 10000
+
+    file1 = in_path + '/hydro_' + '{0:05d}'.format(no1) + '.dat'
+    file2 = dir_path + '/hydro_' + '{0:05d}'.format(no2) + '.in'
+
+    subprocess.call(['cp', file1, file2])
+
+exit()
