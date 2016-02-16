4b763ea8cc56a70454952e66b4fd9a2c4291cafe
Nobuya Nishimura <nishimura.nobuya@gmail.com>
2016-02-16 18:38:09 +0000

    update

--- yes_map/set_calc.py
+++ yes_map/set_calc.py
@@ -4,35 +4,37 @@ import subprocess
 import sys
 
 
-proc = subprocess.Popen(['ls', './res'], stdout = subprocess.PIPE, stderr=subprocess.PIPE)
+proc = subprocess.Popen(['ls', './res'],
+                        stdout = subprocess.PIPE,
+                        stderr = subprocess.PIPE)
 
 stdout_data = proc.communicate()
 
 list = stdout_data[0].split()
 
-for line in list:
-    print(line)
 
-exit()
+for mdl in range(len(list)):
 
-n_pt = []; f_pt = []
+    n_pt = []; f_pt = []
 
-for line in open(sys.argv[1]):
-    dat = line.split()
-    ndat = (len(dat) - 6) /2
-    tmp1 = []; tmp2 =[]
-    for i in range(ndat):
-        tmp1.append(dat[6 + 2*i])
-        tmp2.append(dat[6 + 2*i + 1])
+    for line in open('./res/' + list[mdl]):
+        dat = line.split()
+        ndat = (len(dat) - 6) /2
+        tmp1 = []; tmp2 =[]
+        for i in range(ndat):
+            tmp1.append(dat[6 + 2*i])
+            tmp2.append(dat[6 + 2*i + 1])
 
-    n_pt.append(tmp1)
-    f_pt.append(tmp2)
+        n_pt.append(tmp1)
+        f_pt.append(tmp2)
 
-out = open('./pt_list.dat', 'w')
+    
+    name = list[mdl].split('_')[-1]
+    out = open('./pt_list_' + name, 'w')
 
-for i in range(len(n_pt)):
-    out.write('{0:>10}'.format(n_pt[i][0]) + '\n')
+    for i in range(len(n_pt)):
+        out.write('{0:>10}'.format(n_pt[i][0]) + '\n')
 
-out.close()
+    out.close()
 
 exit()
